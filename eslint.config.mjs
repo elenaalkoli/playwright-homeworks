9b57bb226fedec4bcb82b3a85319ffef7838ecce
elenaalkoli <alkolielena@gmail.com>
2025-10-07 22:02:05 +0300

    initial Playwright setup with TS, ESLint, Prettier, Husky

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint-define-config";
+
+export default defineConfig({
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+});
