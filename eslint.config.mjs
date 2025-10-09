5991f30af667de438783977587f99905c5115444
elenaalkoli <alkolielena@gmail.com>
2025-10-09 15:12:09 +0300

    Add smoke registration tests on /demo-login-form

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,24 @@
-import { defineConfig } from "eslint-define-config";
+import { defineConfig } from 'eslint-define-config';
 
 export default defineConfig({
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   env: {
     node: true,
     es2022: true,
     jest: true,
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
-    "prettier/prettier": ["error"],
+    'prettier/prettier': ['error'],
   },
 });
