fcd09ed4c00eb91251d365f8908e1f4e4977c173
elenaalkoli <alkolielena@gmail.com>
2025-10-29 01:05:05 +0300

    added solutions for task-1 hw-22

---
+++ src/ui/pages/login.page.ts
@@ -0,0 +1,29 @@
+// Написать Page Object класс для страницы Sign In:
+//   - email input
+//   - password input
+//   - login button
+//   - fillCredentials method
+//   - click on login button method
+
+import { BasePage } from './base.page';
+export class LoginPage extends BasePage {
+  readonly emailInput = this.page.locator('#emailinput');
+  readonly passwordInput = this.page.locator('#passwordinput');
+  readonly loginButton = this.page.getByRole('button', { name: 'Login' });
+  readonly title = this.page.locator('p.lead');
+  readonly uniqueElement = this.title;
+
+  async fillCredentials(email: string, password: string) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+  }
+
+  async clickLoginButton() {
+    await this.loginButton.click();
+  }
+
+  async login(email: string, password: string) {
+    await this.fillCredentials(email, password);
+    await this.clickLoginButton();
+  }
+}
